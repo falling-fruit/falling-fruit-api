09302a5e4ff50130346af97781cbff1f4ef88b89
ezwelty <ethan.welty@gmail.com>
2021-03-10 21:59:04 -0700

    Pin to postgres-date=1.0.0
    postgres-date=1.0.7 required dates to be cast to strings in SQL query.

--- reviews.js
+++ reviews.js
@@ -87,8 +87,7 @@ reviews.list = function (req, res) {
       function(callback){ common.check_api_key(req,client,callback) },
       function(callback){
         client.query("SELECT id, location_id, user_id, comment, observed_on, \
-                      TO_CHAR(created_at, 'YYYY-MM-DD\"T\"HH24:MI:SS.MS\"Z\"') as created_at, \
-                      TO_CHAR(updated_at, 'YYYY-MM-DD\"T\"HH24:MI:SS.MS\"Z\"') as updated_at, \
+                      created_at, updated_at, \
                       photo_file_name, fruiting, quality_rating, yield_rating, author, photo_caption \
                       FROM observations WHERE location_id=$1;",
                      [id],function(err, result) {
