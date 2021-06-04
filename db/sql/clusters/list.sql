a72804de8a8e777fc538daed90dc39cc988f908f
ezwelty <ethan.welty@gmail.com>
2021-06-04 16:18:22 +0200

    Fix cluster centerpoint calculation

--- db/sql/clusters/list.sql
+++ db/sql/clusters/list.sql
@@ -5,7 +5,7 @@ FROM (
     ST_Transform(ST_SetSRID(ST_POINT(
       SUM(count * x) / SUM(count),
       SUM(count * y) / SUM(count)
-    ), 900913), 4326) as center
+    ), 3857), 4326) as center
   FROM clusters
   WHERE ${where:raw}
   GROUP BY geohash
