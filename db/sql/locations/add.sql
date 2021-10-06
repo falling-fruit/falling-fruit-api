f1d7fcc34033c7a064bc0444ac7739c14395afbe
ezwelty <ethan.welty@gmail.com>
2021-10-06 10:58:36 +0200

    Always return location muni and import_id

--- db/sql/locations/add.sql
+++ db/sql/locations/add.sql
@@ -10,22 +10,24 @@ WITH l AS (
     ${access}, ${description}, ${unverified}
   )
   RETURNING
-    id, lat, lng, type_ids, user_id, author,
+    id, lat, lng, type_ids, user_id, l.import_id, author,
     created_at, updated_at,
     address, city, state, country,
     season_start, season_stop, no_season,
     access,
     description,
+    muni,
     unverified
 )
 SELECT
-  l.id, l.lat, l.lng, l.type_ids, l.user_id,
+  l.id, l.lat, l.lng, l.type_ids, l.import_id, l.user_id,
   COALESCE(l.author, u.name) as author,
   l.created_at, l.updated_at,
   l.address, l.city, l.state, l.country,
   l.season_start, l.season_stop, l.no_season,
   l.access,
   l.description,
+  l.muni,
   l.unverified
 FROM l
 LEFT JOIN users u
