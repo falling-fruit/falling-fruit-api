2d9c210b27b821270ab9335bf5f062a49dd7622e
ezwelty <ethan.welty@gmail.com>
2021-10-06 11:06:48 +0200

    Use gzip compression

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const cors = require('cors')
 const multer = require('multer')
 const uploads = multer({ dest: 'uploads' })
+const compression = require('compression')
 
 // Constants
 const PORT = 3300
@@ -11,6 +12,7 @@ const BASE = '/test-api/0.3'
 
 // Configuration
 const app = express()
+app.use(compression())
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
