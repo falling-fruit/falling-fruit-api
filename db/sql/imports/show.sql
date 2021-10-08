38d734b03a619fe10de57a0ae1a20a22d54ad99f
ezwelty <ethan.welty@gmail.com>
2021-10-08 21:31:27 +0200

    Add location count to imports

--- db/sql/imports/show.sql
+++ db/sql/imports/show.sql
@@ -1,5 +1,13 @@
 SELECT
   id, created_at, updated_at,
-  url, name, comments, muni, license
+  url, name, comments, muni, license,
+  location_count
 FROM imports
+  LEFT JOIN (
+    SELECT import_id, COUNT(*) as location_count
+    FROM locations
+    WHERE import_id = ${id}
+    GROUP BY import_id
+  ) as l
+  ON id = l.import_id
 WHERE id = ${id}
