6cf7e4303388357ba13ceff87062ee9f77d25760
ezwelty <ethan.welty@gmail.com>
2021-10-20 10:13:54 +0200

    Fix error in list locations SQL

--- db/sql/locations/listphoto.sql
+++ db/sql/locations/listphoto.sql
@@ -1,10 +1,14 @@
-SELECT DISTINCT ON (locations.id)
+SELECT
   locations.id, lng, lat, type_ids ${distance.column:raw},
   o.id as review_id, o.photo_file_name
 FROM locations
-  LEFT JOIN observations o
+  LEFT JOIN (
+    SELECT DISTINCT ON (location_id)
+      id, location_id, photo_file_name
+    FROM observations WHERE photo_file_name IS NOT NULL
+    ORDER BY location_id, created_at DESC
+  ) as o
   ON o.location_id = locations.id
-  AND o.photo_file_name IS NOT NULL
 WHERE ${where:raw}
 ${distance.order:raw}
 LIMIT ${limit}
